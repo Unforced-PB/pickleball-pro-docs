0ba6ea7794fed6b4f1225f9d8e5721db49de8507
Eric Romrell <eric.romrell0@walmart.com>
2023-07-31 14:56:26 -0700

    Removed local api spec and pointed to gist url

--- docs/dist/swagger-initializer.js
+++ docs/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://gist.githubusercontent.com/romrell4/e0939c2ed8dcffeb4c55f863548706e0/raw/2a49ebeeff7e70308802f50b1992dd967289d005/openApi.yml",
+    url: "https://gist.githubusercontent.com/romrell4/e0939c2ed8dcffeb4c55f863548706e0/raw",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
